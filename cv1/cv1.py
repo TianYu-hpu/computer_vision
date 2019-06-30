cd7714c32f4f650c37ce0a8d47d3b5bad62bca69
TianYu <tianyu.hpu@gmail.com>
2019-06-30 23:33:51 +0800

    计算机视觉导论

---
+++ cv1/cv1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import cv2
+
+image = cv2.imread("/home/tianyu/software/IDEAProjects/computer_vision/cv1/lena.jpg")
+cv2.imshow("hello,World", image)
+cv2.waitKey()
+cv2.destroyAllWindows()
